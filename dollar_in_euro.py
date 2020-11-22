52b18bfc20724a082feef5b7eb298ec42be4c7f5
Eruscha <73792963+Eruscha@users.noreply.github.com>
2020-11-22 17:42:40 +0100

    first upload

---
+++ dollar_in_euro.py
@@ -0,0 +1,12 @@
+# Umrechnungsfaktor
+doll = 0.84
+
+# Eingabe der Summe in Dollar
+print("Bitte geben Sie die Summe in US-Dollarn an:")
+xi = float(input())
+
+# Umrechnung
+xcm = xi * doll
+
+# Ausgabe
+print(xi, "US-Dollar sind", xcm, "Euro")
